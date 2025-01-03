9369240d5b3733051f6033088ff412047dea181d
CinnlShiro <cinnlight@gmail.com>
2025-01-02 21:24:58 -0700

    Adds AuthenticationError for GraphQL, modifies signToken for graphql

--- Develop/server/src/services/auth.ts
+++ Develop/server/src/services/auth.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import { GraphQLError } from 'graphql';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -31,9 +32,15 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
   }
 };
 
-export const signToken = (username: string, email: string, _id: unknown) => {
+export const AuthenticationError = new GraphQLError('Could not authenticate user.', {
+  extensions: {
+    code: 'UNAUTHENTICATED',
+  },
+});
+
+export const signToken = ({ username, email, _id }: { username: string; email: string; _id: unknown }) => {
   const payload = { username, email, _id };
   const secretKey = process.env.JWT_SECRET_KEY || '';
-
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  const expiration = '2h';
+  return jwt.sign({ data: payload }, secretKey, { expiresIn: expiration });
 };
