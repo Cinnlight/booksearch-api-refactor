6140f22e1f01069e59cb59a345e97a61282d2c71
CinnlShiro <cinnlight@gmail.com>
2025-01-06 19:24:54 -0700

    Adds createUser and login routes - working

--- Develop/server/schema/resolvers.js
+++ Develop/server/schema/resolvers.js
@@ -5,6 +5,30 @@ const secret = process.env.JWT_SECRET || 'vewwysecwet';
 const expiration = process.env.JWT_EXPIRATION || '2h';
 
 const resolvers = {
+  Mutation: {
+    createUser: async (_, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { user, token };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
