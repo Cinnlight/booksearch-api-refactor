8a2c4cc1105f314300e42affb5c90db2ab9b10ce
Cinnlight <cinnlight@gmail.com>
2025-01-08 10:21:11 -0700

    Provides temp fix to improper .env data fetching

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -3,8 +3,26 @@ require('dotenv').config();
 const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
-const secret = process.env.JWT_SECRET_KEY;
-const expiration = process.env.JWT_EXPIRATION;
+const secret = process.env.JWT_SECRET_KEY || "vewwysecwet";
+const expiration = process.env.JWT_EXPIRATION || "2h";
+
+
+
+const authMiddleware = ({ req }) => {
+  const token = req.headers.authorization || '';
+  
+  if (token) {
+    try {
+      // Verify the token and attach the user to the context
+      const user = jwt.verify(token.split(' ')[1], secret);
+      return { user };
+    } catch (err) {
+      throw AuthenticationError;
+    }
+  }
+  
+  return {};
+};
 
 module.exports = {
   AuthenticationError: new GraphQLError('Could not authenticate user.', {
@@ -12,6 +30,7 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
+  authMiddleware,
   signToken: function ({ email, username, _id }) {
     const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
